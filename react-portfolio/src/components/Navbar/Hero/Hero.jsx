ce32d8a91fcef4438c9cf62dd4f80035be5c2339
SangeethIT19958170 <129158915+SangeethIT19958170@users.noreply.github.com>
2023-12-26 23:22:44 +0530

    Hero section.

---
+++ react-portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Hero.module.css';
+import { getImageUrl } from '../../../utils';
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>
+                Hi, I'm Sangeeth
+            </h1>
+            <p className={styles.description}>
+            I'm a full-stack developer with 6 months experience in full-stack web development. Reach out if you'd like to hire me!
+            </p>
+            <a href='mailto:sriharisangeeth513@gmail.com' className={styles.contactBtn}>Contact Me</a>
+        </div>
+        <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt='My Pic'></img>
+        <div className={styles.topblur}/>
+        <div className={styles.bottomblur}/>
+    </section>
+  )
+}
+
+export default Hero
